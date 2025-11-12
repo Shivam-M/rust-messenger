733ca0169fe4ae54dc84756a3120df37885936ae
Shivam-M <shivammann0@gmail.com>
2025-11-12 16:43:59 +0000

    get server address/port from command line args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::env;
 
-static SERVER_IP: &str = "127.0.0.1";
-static SERVER_PORT: u16 = 5000;
+static DEFAULT_SERVER_ADDRESS: &str = "127.0.0.1";
+static DEFAULT_SERVER_PORT: u16 = 5000;
 
 fn main() {
-    println!("Attempting to connect to server at {SERVER_IP}:{SERVER_PORT}...");
+    let server_address = env::args().nth(1).unwrap_or_else(|| DEFAULT_SERVER_ADDRESS.to_string());
+    let port: u16 = env::args().nth(2)
+        .unwrap_or_else(|| DEFAULT_SERVER_PORT.to_string())
+        .parse()
+        .unwrap_or_else(|_| {
+            eprintln!("Invalid port number provided - using default {DEFAULT_SERVER_PORT}");
+            DEFAULT_SERVER_PORT
+        });
+
+    println!("Attempting to connect to server at {server_address}:{port}...");
 }
