a046c1026dc4f394fd0ba07f4aa7d70d563a63af
Shivam-M <shivammann0@gmail.com>
2025-11-13 11:49:00 +0000

    add username and message handling to the python server

--- broadcaster.py
+++ broadcaster.py
@@ -1,5 +1,6 @@
 from socket import *
 import select
+import json
 
 HOST = "0.0.0.0"
 PORT = 4999
@@ -13,7 +14,7 @@ print(f"Listening on {HOST}:{PORT}...")
 sockets_list = [server_socket]
 clients = {}
 
-def broadcast(message, sender_socket):
+def broadcast(message):
     for client_socket in clients:
         try:
             client_socket.send(message)
@@ -22,29 +23,47 @@ def broadcast(message, sender_socket):
             sockets_list.remove(client_socket)
             del clients[client_socket]
 
+def get_username(sender_socket):
+    return clients.get(sender_socket, "unknown")
+
+def set_username(username, sender_socket):
+    if len(username) > 16 or " " in username:
+        return False
+    clients[sender_socket] = username
+
 while True:
     try:
         read_sockets, _, _ = select.select(sockets_list, [], [])
 
-        for sockfd in read_sockets:
+        for sockfd in list(read_sockets):
             if sockfd == server_socket:
                 client_socket, client_address = server_socket.accept()
                 sockets_list.append(client_socket)
-                clients[client_socket] = client_address
-                print("Client {0}:{1} connected".format(*clients[client_socket]))
+                clients[client_socket] = "unknown"
+                print(f"Client {client_address} connected")
             else:
                 try:
                     message = sockfd.recv(1024)
                     if not message:
-                        print("Client {0}:{1} disconnected".format(*clients[sockfd]))
+                        print(f"Client {sockfd} disconnected")
                         sockets_list.remove(sockfd)
                         del clients[sockfd]
                         continue
                     print(f"Received message from {clients[sockfd]}: {message.decode()}")
-                    broadcast(message, sockfd)
-                except:
+                    try:
+                        json_data = json.loads(message.decode())
+                        if json_data["data-type"] == "message":
+                            if json_data.get("content"):
+                                json_data["username"] = get_username(sockfd)
+                                broadcast(json.dumps(json_data).encode())
+                        elif json_data["data-type"] == "username":
+                            set_username(json_data["username"], sockfd)
+                            sockfd.send(message)
+                    except Exception as e:
+                        print(e)
+                except Exception as e:
+                    print(e)
                     sockets_list.remove(sockfd)
                     del clients[sockfd]
     except Exception as e:
         print(e)
-        continue
